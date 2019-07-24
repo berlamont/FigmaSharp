dec83264921600a7c25745ebbaf5ba3a8e7f955c
Jose Medrano <josmed@microsoft.com>
2019-07-24 21:32:21 +0200

    Refactoring of some code in Game

--- samples/Game.Cocoa/GameWindow.cs
+++ samples/Game.Cocoa/GameWindow.cs
@@ -6,37 +6,88 @@ using FigmaSharp.Services;
 using FigmaSharp.Cocoa;
 using System.Collections.Generic;
 using System.Linq;
-using Foundation;
 using LiteForms;
 using LiteForms.Cocoa;
 
 namespace Game.Cocoa
 {
-    public class GameWindow : Window
+	enum PlayerMovement
+	{
+		Left, Up, Right, Down
+	}
+
+	public class GameWindow : Window
     {
         const int WalkModifier = 13;
       
-        ImageView[] spikesTiles;
-		ImageView[] wallTiles;
+        IImageView[] spikesTiles;
+		IImageView[] wallTiles;
 
-        List<ImageView> gemsTiles;
-        List<ImageView> heartTiles;
-        Label pointsLabel;
-		ImageView playerTile;
+        List<IImageView> gemsTiles;
+        List<IImageView> heartTiles;
+        ILabel pointsLabel;
+		IImageView playerTile;
         int points = 0;
 
-		void MovePlayer(Point point)
-		{
-			if (PlayerPositionCollidesWithWall(point))
-				return;
-			playerTile.SetPosition( point.X, point.Y);
+		Point startingPoint;
+		IMusicPlayer backgroundMusic;
+		IMusicPlayer coinSound;
+		IMusicPlayer gameOverSound;
 
-			Refresh();
+        public GameWindow(Rectangle rect) : base(rect)
+        {
+			Resizable = false;
+
+            //we get the default basic view converters from the current loaded toolkit
+            var converters = FigmaSharp.AppContext.Current.GetFigmaConverters();
+
+            //in this case we want use a remote file provider (figma url from our document)
+            var fileProvider = new FigmaRemoteFileProvider();
+            fileProvider.Load("Jv8kwhoRsrmtJDsSHcTgWGYu");
+
+            //we initialize our renderer service, this uses all the converters passed
+            //and generate a collection of NodesProcessed which is basically contains <FigmaModel, IView, FigmaParentModel>
+            var rendererService = new FigmaViewRendererService(fileProvider, converters);
+
+			//play background music
+			backgroundMusic = new MusicPlayer ("Background", "mp3");
+			backgroundMusic.Play(-1);
+
+			coinSound = new MusicPlayer("Coin", "mp3");
+			gameOverSound = new MusicPlayer("GameOver", "mp3");
+
+			//we want load the entire level 1
+			IView view = rendererService.RenderByName <IView>("Level1");
+			Content = view;
+
+            playerTile = rendererService.FindViewStartsWith<IImageView>("Player");
+
+            startingPoint = playerTile.Allocation.Origin;
+
+            pointsLabel = rendererService.FindViewByName<ILabel>("Points");
+            gemsTiles = rendererService.FindViewsStartsWith<IImageView>("Gem")
+                .ToList ();
+            wallTiles = rendererService.FindViewsStartsWith<IImageView>("Tile")
+                .ToArray();
+			spikesTiles = rendererService.FindViewsStartsWith<IImageView>("Spikes")
+				.ToArray();
+			heartTiles = rendererService.FindViewsStartsWith<IImageView>("Heart")
+				.OrderBy(s => s.Allocation.X)
+				.ToList();
+			WorldUpdate();
 		}
 
-		bool PlayerPositionCollidesWithWall(Point point)
+		void MovePlayerToPoint(Point point)
 		{
-			var playerPosition = new Rectangle (point, playerTile.Size);
+			if (IsPlayerPositionCollidingWithAnyWall (point))
+				return;
+			playerTile.SetPosition(point.X, point.Y);
+			WorldUpdate();
+		}
+
+		bool IsPlayerPositionCollidingWithAnyWall (Point point)
+		{
+			var playerPosition = new Rectangle(point, playerTile.Size);
 			foreach (var gem in wallTiles)
 			{
 				if (gem.Allocation.IntersectsWith(playerPosition))
@@ -45,7 +96,7 @@ namespace Game.Cocoa
 			return false;
 		}
 
-		void Refresh()
+		void WorldUpdate()
 		{
 			var playerPosition = playerTile.Allocation;
 			//if user is in a 
@@ -53,7 +104,7 @@ namespace Game.Cocoa
 			{
 				if (spike.Allocation.IntersectsWith(playerPosition))
 				{
-					PlayerDied();
+					PerformPlayerDied();
 					return;
 				}
 			}
@@ -72,9 +123,9 @@ namespace Game.Cocoa
 			pointsLabel.Text = points.ToString();
 		}
 
-		void PlayerDied()
+		void PerformPlayerDied()
 		{
-			playerTile.SetPosition (startingPoint);
+			playerTile.SetPosition(startingPoint);
 			var lastLive = heartTiles.FirstOrDefault();
 			if (lastLive != null)
 			{
@@ -84,91 +135,45 @@ namespace Game.Cocoa
 			gameOverSound.Play();
 		}
 
-		protected override void OnKeyDownPressed(object sender, NSEvent theEvent)
+		void MovePlayer(PlayerMovement playerMovement)
 		{
-			if (theEvent.KeyCode == (ushort)NSKey.LeftArrow)
+			Point position = playerTile.Allocation.Origin;
+			switch (playerMovement)
 			{
-				var frame = playerTile.Allocation;
-				MovePlayer(new Point(frame.X - WalkModifier, frame.Y));
-				return;
+				case PlayerMovement.Left:
+					position.X -= WalkModifier;
+					break;
+				case PlayerMovement.Right:
+					position.X += WalkModifier;
+					break;
+				case PlayerMovement.Up:
+					position.Y -= WalkModifier;
+					break;
+				default:
+					// PlayerMovement.Bottom
+					position.Y += WalkModifier;
+					break;
 			}
-
-			if (theEvent.KeyCode == (ushort)NSKey.RightArrow)
-			{
-				var frame = playerTile.Allocation;
-				MovePlayer(new Point(frame.X + WalkModifier, frame.Y));
-				return;
-			}
-
-			if (theEvent.KeyCode == (ushort)NSKey.UpArrow)
-			{
-				var frame = playerTile.Allocation;
-				MovePlayer(new Point(frame.X, frame.Y - WalkModifier));
-				return;
-			}
-
-			if (theEvent.KeyCode == (ushort)NSKey.DownArrow)
-			{
-				var frame = playerTile.Allocation;
-				MovePlayer(new Point(frame.X, frame.Y + WalkModifier));
-				return;
-			}
+			MovePlayerToPoint(position);
 		}
 
-		Point startingPoint;
-        AVFoundation.AVAudioPlayer backgroundMusic;
-        AVFoundation.AVAudioPlayer coinSound;
-        AVFoundation.AVAudioPlayer gameOverSound;
-
-        public GameWindow(Rectangle rect) : base(rect)
-        {
-            //we get the default basic view converters from the current loaded toolkit
-            var converters = FigmaSharp.AppContext.Current.GetFigmaConverters();
-
-            //in this case we want use a remote file provider (figma url from our document)
-            var fileProvider = new FigmaRemoteFileProvider();
-            fileProvider.Load("Jv8kwhoRsrmtJDsSHcTgWGYu");
-
-            //we initialize our renderer service, this uses all the converters passed
-            //and generate a collection of NodesProcessed which is basically contains <FigmaModel, IView, FigmaParentModel>
-            var rendererService = new FigmaViewRendererService(fileProvider, converters);
-
-            //play background music
-            var backgroundMusicPath = new NSUrl(NSBundle.MainBundle.PathForResource("Background", "mp3"));
-            backgroundMusic = AVFoundation.AVAudioPlayer.FromUrl(backgroundMusicPath, out NSError error);
-            backgroundMusic.NumberOfLoops = -1;
-            backgroundMusic.Play();
-
-            var gameOverSoundPath = new NSUrl(NSBundle.MainBundle.PathForResource("GameOver", "mp3"));
-            gameOverSound = AVFoundation.AVAudioPlayer.FromUrl(gameOverSoundPath, out error);
-         
-            var coinMusicPath = new NSUrl(NSBundle.MainBundle.PathForResource("Coin", "mp3"));
-            coinSound = AVFoundation.AVAudioPlayer.FromUrl(coinMusicPath, out error);
-
-			//we want load the entire level 1
-			IView view = rendererService.RenderByName <IView>("Level1");
-			Content = view;
-
-            playerTile = rendererService.FindViewStartsWith<ImageView>("Player");
-
-            startingPoint = playerTile.Allocation.Origin;
-
-            pointsLabel = rendererService.FindViewByName<Label>("Points");
-
-            gemsTiles = rendererService.FindViewsStartsWith<ImageView>("Gem")
-                .ToList ();
-
-            wallTiles = rendererService.FindViewsStartsWith<ImageView>("Tile")
-                .ToArray();
-
-			spikesTiles = rendererService.FindViewsStartsWith<ImageView>("Spikes")
-				.ToArray();
-
-			heartTiles = rendererService.FindViewsStartsWith<ImageView>("Heart")
-				.OrderBy(s => s.Allocation.X)
-				.ToList();
-
-			Refresh();
+		protected override void OnKeyDownPressed(object sender, Key args)
+		{
+			switch (args)
+			{
+				case Key.LeftArrow:
+					MovePlayer(PlayerMovement.Left);
+					break;
+				case Key.RightArrow:
+					MovePlayer(PlayerMovement.Right);
+					break;
+				case Key.UpArrow:
+					MovePlayer(PlayerMovement.Up);
+					break;
+				case Key.DownArrow:
+					MovePlayer(PlayerMovement.Down);
+					break;
+			}
 		}
-    }
+	}
 }
