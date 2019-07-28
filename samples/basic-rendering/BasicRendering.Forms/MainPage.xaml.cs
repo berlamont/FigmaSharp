a86ffd9fc7641660c248ea39c4607b264b5a6a79
Jose Medrano <josmed@microsoft.com>
2019-07-28 13:05:04 +0200

    Adds a minor navigation in Basic rendering example

--- samples/basic-rendering/BasicRendering.Forms/MainPage.xaml.cs
+++ samples/basic-rendering/BasicRendering.Forms/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using FigmaSharp.Forms;
 using FigmaSharp.Services;
 using LiteForms;
 using Xamarin.Forms;
+using System.Linq;
 
 namespace BasicRendering.Forms
 {
@@ -14,20 +15,43 @@ namespace BasicRendering.Forms
     public partial class MainPage : ContentPage
     {
 		const string fileName = "NTCS6WenCj7JrvIv5Raeaq";
-
-        public MainPage()
+		FigmaViewRendererService renderer;
+		public MainPage()
         {
             InitializeComponent();
 
 			var converters = FigmaSharp.AppContext.Current.GetFigmaConverters();
+			converters = converters.Concat(
+				new FigmaViewConverter[] {
+					new LoginButtonConverter(),
+					new LoginTextFieldConverter()
+				}).ToArray ();
+
 			var fileProvider = new FigmaRemoteFileProvider();
 			fileProvider.Load(fileName);
 
-			var renderer = new FigmaViewRendererService(fileProvider, converters);
+			renderer = new FigmaViewRendererService(fileProvider, converters);
 
 			var mainScreen = renderer.RenderByPath<IView>(new FigmaViewRendererServiceOptions(), "Log-In Page");
 			BackgroundColor = Xamarin.Forms.Color.Black;
 			Content = mainScreen.NativeObject as AbsoluteLayout;
+
+			LoginButton = renderer.FindViewByName<IView>("LoginButton").NativeObject as Xamarin.Forms.Button;
+			LoginButton.Pressed += Button_Pressed;
+
+			EmailTextField = renderer.FindViewByName<IView>("EmailTextField").NativeObject as Xamarin.Forms.Entry;
+			EmailTextField = renderer.FindViewByName<IView>("PasswordTextField").NativeObject as Xamarin.Forms.Entry;
+
 		}
-    }
+
+		Xamarin.Forms.Entry EmailTextField, PasswordTextField;
+		Xamarin.Forms.Button LoginButton;
+
+		private void Button_Pressed(object sender, System.EventArgs e)
+		{
+			var mainScreen = renderer.RenderByPath<IView>(new FigmaViewRendererServiceOptions(), "Home");
+			Content = mainScreen.NativeObject as AbsoluteLayout;
+			BackgroundColor = Xamarin.Forms.Color.White;
+		}
+	}
 }
